a110956368eef614d944aafba97961b55ab8262c
Marcelo <marcelo@MacBook-Air-de-Marcelo.local>
2018-10-26 00:07:48 -0300

    Commit inicial

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,8 @@
+/**
+ * Arquivo para adicionar alguns estilos para a StatusBar e ActionBar
+ * Será importada no /src/index.js
+ */
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor("#1A237E");
+StatusBar.setBarStyle("light-content");
